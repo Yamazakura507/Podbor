fe2534f33cbf99496582e6339481eaca62f74533
Yamazakura507 <88948531+Yamazakura507@users.noreply.github.com>
2024-10-17 11:39:03 +0300

    Добавление подписи журнала планирования

--- Podbor/Podbor/Podbor/obj/Debug/net8.0-android/Podbor.AssemblyInfo.cs
+++ Podbor/Podbor/Podbor/obj/Debug/net8.0-android/Podbor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Podbor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2a8ceb832650e76b8a4b43d125815678c552beba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6b9ea54e6db95c3a14e1b2afd633be91c6c3934a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Podbor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Podbor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
