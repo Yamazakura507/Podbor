f0fc34660b36843746760a3ffcc6c903e61df3fb
Yamazakura507 <88948531+Yamazakura507@users.noreply.github.com>
2024-10-17 12:37:56 +0300

    Результат

---
+++ Podbor/Podbor/Podbor/View/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podbor.View
+{
+    public class Result
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public decimal Profit
+        {
+            get;
+            set;
+        }
+        public decimal ProfitForEvrifing
+        {
+            get;
+            set;
+        }
+        public decimal DebtorSum
+        {
+            get;
+            set;
+        }
+    }
+}
--- Podbor/Podbor/Podbor/obj/Debug/net8.0-windows10.0.26100.0/win10-x64/Podbor.AssemblyInfo.cs
+++ Podbor/Podbor/Podbor/obj/Debug/net8.0-windows10.0.26100.0/win10-x64/Podbor.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Podbor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe2534f33cbf99496582e6339481eaca62f74533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dfe952bd3f17dd78f1a63dd9df3e84af26907315")]
 [assembly: System.Reflection.AssemblyProductAttribute("Podbor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Podbor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
